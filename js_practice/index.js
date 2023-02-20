e2dc4cca6afef393564ffd9a01ed81e34bef72a2
Som Gupta <somgupta50@gmail.com>
2023-02-20 18:30:22 +0530

    JS

---
+++ js_practice/index.js
@@ -0,0 +1,17 @@
+console.log('it works 😁')
+
+updateColor = function(){
+    console.log('great')
+}
+
+function dosomething(){
+    alert('i am doing something')
+}
+
+animate_button = () => {
+    console.log('i am animating the button')
+}
+
+changeMessage = (e) => {
+    e.target.innerText="button clicked"
+}
